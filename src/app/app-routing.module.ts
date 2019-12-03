ea368f40b51c26784dc6630935eb47bc34e52054
CuratoloG <34574535+CuratoloG@users.noreply.github.com>
2019-12-03 18:38:12 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LetsPlayComponent } from './lets-play/lets-play.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'buttonLetsPlay', component: LetsPlayComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LetsPlayComponent } from './lets-play/lets-play.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LetsPlayComponent
   ],
   imports: [
     BrowserModule,
