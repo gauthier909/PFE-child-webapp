cdf8cb1106e735a07ec01b36412b603571317e4f
pjalbrz16 <48532936+pjalbrz16@users.noreply.github.com>
2019-12-11 14:29:01 +0100

    reglage souci socket

--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -1,25 +1,27 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client'
-import {Filtre} from '../app/partie/filtre'
+import { Filtre } from '../app/partie/filtre'
 
 const IO_ROUTE = "http://localhost:8081"
 const IO_ROOM = "testRoom"
 
 @Injectable({
     providedIn: 'root'
-  })
+})
 export class SocketService {
     private socket
-    message:{message:[{
-        filtrePositif:string,
-        filtreNegatif:string
-    }]}
-    constructor(){}
-    
+    message: {
+        message: [{
+            filtrePositif: string,
+            filtreNegatif: string
+        }], jeu_id: string
+    }
+    constructor() { }
+
     /**
      * L'initialisation de la connection + ajout des listeners
      */
-    socketInit(){
+    socketInit() {
         this.socket = io(IO_ROUTE)
         this.socket.emit("joinRoom", IO_ROOM)
         this.socket.on("message", (message) => {
@@ -32,7 +34,7 @@ export class SocketService {
      * Permet d'envoyer un message au front-end du professionnel via le backend
      * @param message Le message que l'on souhaite envoyer au front-end du professionnel
      */
-    sendMessage(message: any){
-        this.socket.emit("message", ({room: IO_ROOM, message:message}))
+    sendMessage(message: any) {
+        this.socket.emit("message", ({ room: IO_ROOM, message: message }))
     }
 }
