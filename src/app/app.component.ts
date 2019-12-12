7dece5bcd9e33277134eea81b7f0c00697119de9
pjalbrz16 <48532936+pjalbrz16@users.noreply.github.com>
2019-12-12 12:53:13 +0100

    modif button lancer partie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {SwUpdate} from '@angular/service-worker';
 import { SocketService } from 'src/services/socket.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,12 +14,14 @@ export class AppComponent implements OnInit {
 
   constructor(
     private swUpdate:SwUpdate,
-    private socketService: SocketService
+    private socketService: SocketService,
+    private router: Router
   ){}
 
   ngOnInit(){
     this.socketService.socketInit()
     this.reloadCache();
+    this.router.navigate([''])
   }
 
   //update l'appli des qu'il y a un changment (pwa)
